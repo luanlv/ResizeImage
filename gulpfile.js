945f71d97401fe5af4878ded348141a031c0bc2f
luan <mrvluan@gmail.com>
2015-07-13 14:10:43 +0700

    fix

--- gulpfile.js
+++ gulpfile.js
@@ -1,59 +1,41 @@
-
-  var gulp        = require('gulp');
-  var sass        = require('gulp-sass');
-  var browserSync = require('browser-sync');
-  var sourcemaps = require('gulp-sourcemaps');
+var gulp        = require('gulp');
+var sass        = require('gulp-sass');
+var browserSync = require('browser-sync');
+var sourcemaps = require('gulp-sourcemaps');
 
 // Registering a 'less' task that just compile our LESS files to CSS
+gulp.task('sass', function() {
+  gulp.src('./resources/sass/main.scss')
+      .pipe(sourcemaps.init())
+      .pipe(sass({
+        errLogToConsole: true
+      }))
+      .pipe(sourcemaps.write())
+      .pipe(gulp.dest('./public/stylesheets'));
+});
 
+gulp.task('watch', ['sass'], function () {
+  gulp.watch('./resources/sass/{,*/}*.{scss,sass}', ['sass'])
+});
 
-  gulp.task('watch', ['sass'], function () {
-    gulp.watch('./resources/sass/{,*/}*.{scss,sass}', ['sass'])
+//
+gulp.task('serve', function () {
+  browserSync({
+    // By default, Play is listening on port 9000
+    proxy: 'localhost:9000',
+    // We will set BrowserSync on the port 9001
+    port: 9001,
+    // Reload all assets
+    // Important: you need to specify the path on your source code
+    // not the path on the url
+    files: ['public/stylesheets/*.css',
+      'public/javascripts/*.js',
+      'app/views/{,*/}*.{html, stream}',
+      'app/controllers/{,*/}*.scala',
+      'conf/routes'],
+    open: false
   });
-
-
-  gulp.task('minify-html', function() {
-    var opts = {
-      conditionals: true,
-      spare:true,
-      quotes: true,
-      empty: true,
-      cdata: true,
-      loose: true
-    };
-    return gulp.src('./resources/html/*.{stream,html}')
-        .pipe(minifyHTML(opts))
-        .pipe(gulp.dest('./app/views'));
-  });
-
-  gulp.task('sass', function() {
-    gulp.src('./resources/sass/main.scss')
-        .pipe(sourcemaps.init())
-        .pipe(sass({
-          errLogToConsole: true
-        }))
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest('./public/stylesheets'));
-  });
-
-
-  gulp.task('serve', function () {
-    browserSync({
-      // By default, Play is listening on port 9000
-      proxy: 'localhost:9000',
-      // We will set BrowserSync on the port 9001
-      port: 9001,
-      // Reload all assets
-      // Important: you need to specify the path on your source code
-      // not the path on the url
-      files: ['public/stylesheets/*.css',
-        'public/javascripts/*.js',
-        'app/views/*.html',
-        'app/controllers/{,*/}*.scala',
-        'conf/routes'],
-      open: false
-    });
-  });
+});
 
 // Creating the default gulp task
-  gulp.task('default', ['sass', 'minify-html', 'watch', 'serve']);
+gulp.task('default', ['sass', 'watch', 'serve']);
