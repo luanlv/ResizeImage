a689b71a939d64d572272087d419bf2feb99f644
luan <mrvluan@gmail.com>
2015-08-01 18:10:51 +0700

    fix

--- public/javascripts/multizoom.js
+++ public/javascripts/multizoom.js
@@ -386,8 +386,9 @@ $.fn.addimagezoom = function(options){
   });
 };
 
-if ($('#multizoom1').length)
-{
+
+
+$(document).on('mouseover', '#multizoom1', function() {
   $('#multizoom1').addimagezoom({ // multi-zoom: options same as for previous Featured Image Zoomer's addimagezoom unless noted as '- new'
     descArea: '#description', // description selector (optional - but required if descriptions are used) - new
     speed: 0, // duration of fade in for new zoomable images (in milliseconds, optional) - new
@@ -395,12 +396,13 @@ if ($('#multizoom1').length)
     imagevertcenter: true, // zoomable image centers vertically in its container (optional) - new
     magvertcenter: true, // magnified area centers vertically in relation to the zoomable image (optional) - new
     zoomrange: [5, 10],
-    magnifiersize: [450,450],
+    magnifiersize: [450, 450],
     magnifierpos: 'right',
     cursorshadecolor: '#fdffd5',
     cursorshade: true //<-- No comma after last option!
   });
-}
+});
+
 
 
 
