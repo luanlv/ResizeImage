3449337862db28560d1b2ac1fddb8f7d084d39b1
luan <mrvluan@gmail.com>
2015-08-07 21:00:54 +0700

    fix

--- app/controllers/CdnCtrl.scala
+++ app/controllers/CdnCtrl.scala
@@ -12,27 +12,10 @@ class CdnCtrl extends Controller {
     Ok("clear CDN ok!")
   }
 
-  def setCDN1(name: String) = Action {
+  def setCDN(name: String) = Action {
     val newCdn = "//" + name
     maybeAssetsUrl = Option(newCdn)
     Ok("Set cdn1 to: " + name + " ok!")
   }
 
-  def setCDN2(name: String) = Action {
-    val newCdn = "//" + name
-    maybeAssetsUrl2 = Option(newCdn)
-    Ok("Set cdn2 to: " + name + " ok!")
-  }
-
-  def setCDN3(name: String) = Action {
-    val newCdn = "//" + name
-    maybeAssetsUrl3 = Option(newCdn)
-    Ok("Set cdn2 to: " + name + " ok!")
-  }
-
-  def setCDN4(name: String) = Action {
-    val newCdn = "//" + name
-    maybeAssetsUrl3 = Option(newCdn)
-    Ok("Set cdn2 to: " + name + " ok!")
-  }
 }
--- app/core/Cdn.scala
+++ app/core/Cdn.scala
@@ -4,27 +4,13 @@ import scala.util.Random
 
 object Cdn {
   var maybeAssetsUrl = Option("")
-  var maybeAssetsUrl2 = Option("")
-  var maybeAssetsUrl3 = Option("")
-  var maybeAssetsUrl4 = Option("")
 
   def assetCDN(url: String): String = {
     val result =
       if(url.take(2) == "//")
         url
-      else {
-        val rand = Random.nextDouble()
-        if(rand<0.25)
-          Cdn.maybeAssetsUrl.fold(url)(_ + url)
-        else
-          if(rand<0.5)
-            Cdn.maybeAssetsUrl2.fold(url)(_ + url)
-          else
-            if(rand<0.75)
-              Cdn.maybeAssetsUrl3.fold(url)(_ + url)
-            else
-              Cdn.maybeAssetsUrl4.fold(url)(_ + url)
-      }
+      else
+        Cdn.maybeAssetsUrl.fold(url)(_ + url)
     result
   }
 }
